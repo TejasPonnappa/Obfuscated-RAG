9b966a1d9d5f00aa38238639fc014b40c7b6838b
TejasPonnappa <tejasponnappa9@gmail.com>
2026-08-02 15:34:49 +0530

    fixed evaluate noise logic

--- gan_logic.py
+++ gan_logic.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score  
 
 class DifferentialPrivacyAgent:
     def __init__(self, epsilon: float = 2.0, sensitivity: float = 1.0):
@@ -17,25 +17,20 @@ class FastDiscriminator:
     def __init__(self):
         self.model = LogisticRegression(max_iter=1000)
 
-    def evaluate_noise(self, clean_vectors: np.ndarray, noisy_vectors: np.ndarray) -> float:
-        if clean_vectors.shape[0] < 5:
-            clean_vectors = np.tile(clean_vectors, (5, 1))
-            noisy_vectors = np.tile(noisy_vectors, (5, 1))
-            
-        y_clean = np.zeros(clean_vectors.shape[0])
-        y_noisy = np.ones(noisy_vectors.shape[0])
-
+    def evaluate_noise(self, clean_vectors, noisy_vectors):
+        y_clean = np.zeros(len(clean_vectors))
+        y_noisy = np.ones(len(noisy_vectors))
         X = np.vstack((clean_vectors, noisy_vectors))
         y = np.concatenate((y_clean, y_noisy))
+
+        if len(X) < 10:  # too few to split meaningfully
+            return 0.5  # assume ambiguous, skip evaluation
 
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=0.3, random_state=42, stratify=y
         )
-        
         self.model.fit(X_train, y_train)
-        predictions = self.model.predict(X_test)
-        
-        return accuracy_score(y_test, predictions)
+        return accuracy_score(y_test, self.model.predict(X_test))
 
 def optimize_privacy_budget(clean_vectors: np.ndarray, target_accuracy: float = 0.55):
     """
